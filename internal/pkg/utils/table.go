fbd66b578f2c592ef2bb8d7bb1b2aabb6a4e12c5
asepopov <asepopov@avito.ru>
2025-03-23 03:49:49 +0300

    change migratioon auto

--- internal/pkg/utils/table.go
+++ internal/pkg/utils/table.go
@@ -2,6 +2,13 @@ package utils
 
 import "strings"
 
+// cleanMarkdown удаляет MD разметку заголовков из текста
+func cleanMarkdown(text string) string {
+	// Удаляем символы # в начале строки
+	text = strings.TrimLeft(text, "# ")
+	return text
+}
+
 // CreateTable создает строковое представление таблицы
 func CreateTable(headers []string, rows [][]string) string {
 	var builder strings.Builder
@@ -24,7 +31,9 @@ func CreateTable(headers []string, rows [][]string) string {
 	for _, row := range rows {
 		builder.WriteString("|")
 		for _, cell := range row {
-			builder.WriteString(" " + cell + " |")
+			// Очищаем MD разметку перед добавлением в таблицу
+			cleanCell := cleanMarkdown(cell)
+			builder.WriteString(" " + cleanCell + " |")
 		}
 		builder.WriteString("\n")
 	}
